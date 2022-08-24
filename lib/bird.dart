3f794502f8fc309520db43d0a2522d921e31d2d4
Hidaya El Habti <hadouya24@gmail.com>
2022-08-24 15:17:53 +0100

    UI

---
+++ lib/bird.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MyBird extends StatelessWidget {
+  const MyBird({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 60,
+      width: 60,
+      child: Image.asset('lib/images/bird.png'),
+    );
+  }
+}
---
+++ lib/home_page.dart
@@ -0,0 +1,31 @@
+import 'package:flappy_bird/bird.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            flex: 2,
+            child: Container(
+              color: Colors.blue,
+              child: Center(child: const MyBird()),
+            ),
+          ),
+          Expanded(
+            child: Container(color: Colors.green),
+          ),
+        ],
+      ),
+    );
+  }
+}
